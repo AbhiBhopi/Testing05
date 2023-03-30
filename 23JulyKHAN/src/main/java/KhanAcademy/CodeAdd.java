329b55dab20a31a72f1a6711541ac018b7d7a5a1
yadav <yadav@DESKTOP-BKS66NO>
2023-03-31 00:21:37 +0530

    added

---
+++ 23JulyKHAN/src/main/java/KhanAcademy/CodeAdd.java
@@ -0,0 +1,36 @@
+package KhanAcademy;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+public class CodeAdd {
+	
+	@FindBy(xpath="//input[@class='_q92ef6']")
+	private WebElement codeadd;
+
+	@FindBy(xpath="//button[@type='buttons']")
+	private WebElement continues;
+
+
+	@FindBy(xpath="//a[@role='button']")
+	private WebElement backbutton;
+	public CodeAdd(WebDriver driver)
+	{
+		PageFactory.initElements(driver, this);
+	}
+	public void codeadd1()
+	{
+		codeadd.sendKeys("12345678");
+	}
+	public void continues()
+	{
+		continues.click();
+	}
+	public void back()
+	{
+		backbutton.click();
+	}
+
+}
